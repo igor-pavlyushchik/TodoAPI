3866e4873ba20607a4286c53b707193e617aae23
Igor Pavlyushchik <ipavlyushchik@mobileiron.com>
2024-06-25 18:41:37 +0200

    init project commit

---
+++ src/test/java/epam/ip/todo/TodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package epam.ip.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
